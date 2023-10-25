5dbc6194be0cdd249f68a8df8aba837a68d4ba6c
Edgar <eddyade98@gmail.com>
2023-10-25 12:19:25 +0300

    Completed Assginment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded' , function () {
+    const textParagraph = document.getElementById('text');
+
+    if (textParagraph) {
+        textParagraph.textContent = 'This is really cool!';
+      } else {
+        console.log('Element with ID "text" not found.');
+      }
+})
